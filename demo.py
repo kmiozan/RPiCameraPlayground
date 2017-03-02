f8923a644a83c3a7379b37de6afc4d42824acd7d
26000595 <ozan1616>
2017-03-02 20:05:37 +0300

    Try

--- demo.py
+++ demo.py
@@ -4,31 +4,7 @@ cam=Camera() # //Intializing camera
 time.sleep(5) #//delay for three seconds
 a=cam.getImage()#//capturing the first image
 a.save("a.jpg")
-time.sleep(1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-           ,
-           0)
+time.sleep(10)
 b=cam.getImage()#//capturing the second image after one second
 b.save("b.jpg")
 d=b-a #//subtracting the image pixels
